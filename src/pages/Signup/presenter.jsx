16032ca3872b86fafeb3134d81820cdfdfee5e7a
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-09 00:21:52 +0900

    ユーザー新規作成画面のUI実装

--- src/pages/Signup/presenter.jsx
+++ src/pages/Signup/presenter.jsx
@@ -1,3 +1,31 @@
+import "./SignUp.css";
+
 export const SignUpPagePresenter = () => {
-  return <div>ユーザー作成ページ</div>;
+  return (
+    <div className="signUp">
+      <h2>ユーザー新規作成</h2>
+      <form onSubmit={() => {}}>
+        <div className="form-item">
+          <label>メールアドレス</label>
+          <br />
+          <input type="email" id="email" className="input" />
+        </div>
+        <div className="form-item">
+          <label>名前</label>
+          <br />
+          <input type="text" id="name" className="input" />
+        </div>
+        <div className="form-item">
+          <label>パスワード</label>
+          <br />
+          <input type="password" id="password" className="input" />
+        </div>
+        <div className="signUp-button-container">
+          <button type="button" onClick={() => {}} className="signUp-button">
+            作成
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
