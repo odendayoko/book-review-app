bfdd4bf82abb251abe3011f17e2f0255fdde0a9e
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-09 21:51:03 +0900

    ユーザー新規作成画面のUI作成

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,5 +1,21 @@
+import { useForm } from "react-hook-form";
 import { SignUpPagePresenter } from "./presenter";
 
 export const SignUpPage = () => {
-  return <SignUpPagePresenter />;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <SignUpPagePresenter
+      register={register}
+      handleSubmit={handleSubmit(onSubmit)}
+      onSubmit={onSubmit}
+      errors={errors}
+    />
+  );
 };
--- src/pages/Signup/presenter.jsx
+++ src/pages/Signup/presenter.jsx
@@ -1,27 +1,53 @@
 import "./SignUp.css";
 
-export const SignUpPagePresenter = () => {
+export const SignUpPagePresenter = ({ register, handleSubmit, errors }) => {
   return (
     <div className="signUp">
       <h2>ユーザー新規作成</h2>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <div className="form-item">
           <label htmlFor="email">メールアドレス</label>
           <br />
-          <input type="email" id="email" className="input" />
+          <input
+            type="email"
+            id="email"
+            className="input"
+            {...register("email", { required: "メールアドレスは必須です" })}
+          />
+          {errors.email && (
+            <span className="error-message">{errors.email.message}</span>
+          )}
         </div>
         <div className="form-item">
           <label htmlFor="name">名前</label>
           <br />
-          <input type="text" id="name" className="input" />
+          <input
+            type="text"
+            id="name"
+            className="input"
+            {...register("name", { required: "名前は必須です" })}
+          />
+          {errors.name && (
+            <span className="error-message">{errors.name.message}</span>
+          )}
         </div>
         <div className="form-item">
           <label htmlFor="password">パスワード</label>
           <br />
-          <input type="password" id="password" className="input" />
+          <input
+            type="password"
+            id="password"
+            className="input"
+            {...register("password", {
+              required: "パスワードは必須です",
+            })}
+          />
+          {errors.password && (
+            <span className="error-message">{errors.password.message}</span>
+          )}
         </div>
         <div className="signUp-button-container">
-          <button type="button" onClick={() => {}} className="signUp-button">
+          <button className="signUp-button" type="submit">
             作成
           </button>
         </div>
