5425feca31ebfdf0378ea18a6351c9a062a76253
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-11 19:30:30 +0900

    ログイン画面のUI実装

--- src/pages/Login/presenter.jsx
+++ src/pages/Login/presenter.jsx
@@ -1,3 +1,53 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 export const LoginPagePresenter = () => {
-  return <div>ログインページ</div>;
+  return (
+    <div className="login">
+      <h2>ログイン</h2>
+      <form onSubmit={() => {}}>
+        <div className="form-item">
+          <label htmlFor="email">メールアドレス</label>
+          <br />
+          <input
+            type="email"
+            id="email"
+            className="input"
+            // {...register("email", { required: "メールアドレスは必須です" })}
+          />
+          {/* {errors.email && (
+            <span className="error-message">{errors.email.message}</span>
+          )} */}
+        </div>
+        <div className="form-item">
+          <label htmlFor="password">パスワード</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            className="input"
+            // {...register("password", {
+            //   required: "パスワードは必須です",
+            // })}
+          />
+          {/* {errors.password && (
+            <span className="error-message">{errors.password.message}</span>
+          )} */}
+        </div>
+        <div className="button-container">
+          <button className="login-button" type="submit">
+            ログイン
+          </button>
+        </div>
+      </form>
+      <div className="button-container">
+        <label>はじめての方はこちら</label>
+        <br />
+        <br />
+        <Link to="/SignUp">
+          <button className="navigate-button">ユーザー新規作成</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
