965d2dff4385910fd5c34f282a1ac15eee2a15d5
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-12 14:19:08 +0900

    トップページにページネーションを追加

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SignUpPage } from "./pages/Signup";
 import { LoginPage } from "./pages/Login";
 import { HomePage } from "./pages/Home";
+import { UIProvider } from "@yamada-ui/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <UIProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </UIProvider>
   );
 }
 
--- src/pages/Home/presenter.jsx
+++ src/pages/Home/presenter.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import "./home.css";
+import { Pagination } from "@yamada-ui/pagination";
 
 export const HomePagePresenter = ({ books }) => {
   return (
     <div className="homeContainer">
-      <h2>書籍レビュー一覧</h2>
+      <h2 className="homeContainer__title">書籍レビュー一覧</h2>
       <ul className="bookList">
         {books.map((book) => (
           <li key={book.id} className="bookList__item">
@@ -17,6 +18,17 @@ export const HomePagePresenter = ({ books }) => {
           </li>
         ))}
       </ul>
+      <div className="paginationContainer">
+        <Pagination
+          total={50}
+          variant="solid"
+          size="lg"
+          colorScheme="pink"
+          siblings={2}
+          withEdges
+          onChange={() => {}}
+        />
+      </div>
     </div>
   );
 };
