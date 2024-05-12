5d1b9e262ce3551e7f5a75c9b0f873ded05e103e
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-12 22:58:36 +0900

    ログイン状況によって叩くAPIを変える

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,38 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { HomePagePresenter } from "./presenter";
 import axios from "axios";
+import { AuthContext } from "../../router/AuthContext";
 
 export const HomePage = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+
   const [books, setBooks] = useState([]);
   const [page, setPage] = useState(0);
   const booksPerPage = 10;
 
   useEffect(() => {
+    // pageの変更だけを検知して再フェッチを行う
     fetchBooks(page).then((responseData) => {
       setBooks(responseData);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
   const fetchBooks = async (page) => {
     try {
+      // tokenもAuthContextから返してもいいかも
       const token = localStorage.getItem("token");
-
+      const offset = page * booksPerPage;
+      const headers = {
+        "Content-Type": "application/json",
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      };
+      // ログイン状況によってAPIが異なる
       const response = await axios.get(
-        `https://railway.bookreview.techtrain.dev/books?offset=${
-          page * booksPerPage
-        }`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
+        `https://railway.bookreview.techtrain.dev/${
+          isLoggedIn ? "books" : "public/books"
+        }?offset=${offset}`,
+        { headers }
       );
       return response.data;
     } catch (error) {
