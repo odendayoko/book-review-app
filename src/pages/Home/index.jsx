1910be9e9abbf368f24aed15fa377ea581abd76f
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-12 13:23:32 +0900

    トップページでレビュー一覧を取得して描画する

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
 import { HomePagePresenter } from "./presenter";
+import axios from "axios";
 
 export const HomePage = () => {
-  return <HomePagePresenter />;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    // 仮でpageを0としておく
+    fetchBooks(0).then((responseData) => {
+      setBooks(responseData);
+    });
+  }, []);
+
+  const fetchBooks = async (page) => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await axios.get(
+        `https://railway.bookreview.techtrain.dev/books?offset=${page}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error.response.data.ErrorMessageJP);
+      }
+      throw error;
+    }
+  };
+
+  return <HomePagePresenter books={books} />;
 };
--- src/pages/Home/presenter.jsx
+++ src/pages/Home/presenter.jsx
@@ -1,26 +1,7 @@
 import { Link } from "react-router-dom";
 import "./home.css";
 
-export const HomePagePresenter = () => {
-  const books = [
-    {
-      id: "1",
-      title: "カブトムシ",
-      url: "https://hoge",
-      detail: "少し背の高い",
-      review: "ちょっと怖いよなこの歌詞",
-      reviewer: "aiko",
-    },
-    {
-      id: "2",
-      title: "Pretender",
-      url: "https://pretender",
-      detail: "君は綺麗だ",
-      review: "神曲！",
-      reviewer: "髭男",
-    },
-  ];
-
+export const HomePagePresenter = ({ books }) => {
   return (
     <div className="homeContainer">
       <h2>書籍レビュー一覧</h2>
