dda8b6224dd3817412ac214e9df30310752de66b
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-13 23:44:46 +0900

    レビュー新規投稿ができるようにする

--- src/pages/NewBook/index.jsx
+++ src/pages/NewBook/index.jsx
@@ -1,5 +1,51 @@
+import axios from "axios";
 import { NewBookPagePresenter } from "./presenter";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const NewBookPage = () => {
-  return <NewBookPagePresenter />;
+  const navigate = useNavigate();
+
+  const [formValue, setFormValue] = useState({
+    title: "",
+    url: "",
+    detail: "",
+    review: "",
+  });
+
+  const postBooks = async (token) => {
+    try {
+      await axios.post(
+        "https://railway.bookreview.techtrain.dev/books",
+        formValue,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error.response.data.ErrorMessageJP);
+      }
+      throw error;
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem("token");
+
+    if (token == null) {
+      return;
+    }
+
+    postBooks(token).then(() => {
+      alert("レビューを投稿しました");
+      navigate("/");
+    });
+  };
+
+  return <NewBookPagePresenter onSubmit={handleSubmit} />;
 };
--- src/pages/NewBook/presenter.jsx
+++ src/pages/NewBook/presenter.jsx
@@ -1,5 +1,60 @@
+import { Header } from "../../components/Header";
 import "./newBook.css";
 
-export const NewBookPagePresenter = () => {
-  return <div>新規投稿</div>;
+export const NewBookPagePresenter = ({ onSubmit }) => {
+  return (
+    <>
+      <Header />
+      <div className="profile">
+        <h2>新規レビュー投稿</h2>
+        <form onSubmit={onSubmit}>
+          <div className="formItem">
+            <label htmlFor="title">タイトル</label>
+            <br />
+            <input
+              type="text"
+              id="name"
+              className="input"
+              // onChange={(event) => handleChangeUserName(event.target.value)}
+            />
+          </div>
+          <div className="formItem">
+            <label htmlFor="url">購入サイトのリンク</label>
+            <br />
+            <input
+              type="url"
+              id="url"
+              className="input"
+              // onChange={(event) => handleChangeUserName(event.target.value)}
+            />
+          </div>
+          <div className="formItem">
+            <label htmlFor="url">本の詳細</label>
+            <br />
+            <textarea
+              type="detail"
+              id="detail"
+              className="textarea"
+              // onChange={(event) => handleChangeUserName(event.target.value)}
+            />
+          </div>
+          <div className="formItem">
+            <label htmlFor="url">本のレビュー</label>
+            <br />
+            <textarea
+              type="review"
+              id="review"
+              className="textarea"
+              // onChange={(event) => handleChangeUserName(event.target.value)}
+            />
+          </div>
+          <div className="updateButtonContainer">
+            <button className="updateButton" type="submit">
+              投稿
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
