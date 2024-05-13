e38b066789ce0d2c2d0f378045361a981295a610
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-13 20:33:04 +0900

    ユーザー名を更新できるようにする

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,52 @@
+import axios from "axios";
 import { ProfilePagePresenter } from "./presenter";
+import { useState } from "react";
 
 export const ProfilePage = () => {
-  return <ProfilePagePresenter />;
+  const [formValue, setFormValue] = useState({ name: "defaultName" });
+
+  const updateUser = async (token) => {
+    try {
+      await axios.put(
+        "https://railway.bookreview.techtrain.dev/users",
+        formValue,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error.response.data.ErrorMessageJP);
+      }
+      throw error;
+    }
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem("token");
+
+    if (token == null) {
+      return;
+    }
+
+    updateUser(token).then(() => {
+      alert("プロフィールを更新しました");
+    });
+  };
+
+  const handleChangeUserName = (newName) => {
+    setFormValue({ ...formValue, name: newName });
+  };
+
+  return (
+    <ProfilePagePresenter
+      formValue={formValue}
+      onUpdate={handleUpdate}
+      handleChangeUserName={handleChangeUserName}
+    />
+  );
 };
--- src/pages/Profile/presenter.jsx
+++ src/pages/Profile/presenter.jsx
@@ -1,21 +1,31 @@
 import "./profile.css";
 import { Header } from "../../components/Header";
 
-export const ProfilePagePresenter = () => {
+export const ProfilePagePresenter = ({
+  formValue,
+  onUpdate,
+  handleChangeUserName,
+}) => {
   return (
     <>
       <Header />
       <div className="profile">
         <h2>プロフィール編集</h2>
-        <form>
+        <form onSubmit={onUpdate}>
           <div className="formItem">
             <label htmlFor="name">名前</label>
             <br />
-            <input type="name" id="name" className="input" />
+            <input
+              type="name"
+              id="name"
+              className="input"
+              onChange={(event) => handleChangeUserName(event.target.value)}
+              value={formValue.name}
+            />
           </div>
           <div className="updateButtonContainer">
             <button className="updateButton" type="submit">
-              変更
+              更新
             </button>
           </div>
         </form>
