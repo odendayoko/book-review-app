42f1d57187224d280e74c733f9d1f1099cbee5fa
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-14 22:45:27 +0900

    <Pagination>のcssを有効化

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,15 +1,18 @@
 import { Pagination as YamadaUIPagination } from "@yamada-ui/pagination";
+import { UIProvider } from "@yamada-ui/react";
 
 export const Pagination = ({ onChange, total }) => {
   return (
-    <YamadaUIPagination
-      total={total}
-      variant="solid"
-      size="lg"
-      colorScheme="pink"
-      siblings={2}
-      withEdges
-      onChange={onChange}
-    />
+    <UIProvider>
+      <YamadaUIPagination
+        total={total}
+        variant="solid"
+        size="lg"
+        colorScheme="pink"
+        siblings={2}
+        withEdges
+        onChange={onChange}
+      />
+    </UIProvider>
   );
 };
