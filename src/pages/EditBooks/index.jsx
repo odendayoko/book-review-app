fe763c79f4d348fb87fa6ff38824491745ab8ea3
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-14 23:06:10 +0900

    レビュー編集画面のUI実装

--- src/pages/EditBooks/index.jsx
+++ src/pages/EditBooks/index.jsx
@@ -1,5 +1,55 @@
+import axios from "axios";
 import { EditBooksPagePresenter } from "./presenter";
+import { useNavigate, useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export const EditBooksPage = () => {
-  return <EditBooksPagePresenter />;
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const updateBooks = async (token, params) => {
+    try {
+      await axios.put(
+        `https://railway.bookreview.techtrain.dev/books/${id}`,
+        params,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error.response.data.ErrorMessageJP);
+      }
+      throw error;
+    }
+  };
+
+  const onUpdate = async (formData) => {
+    const token = localStorage.getItem("token");
+
+    if (token == null) {
+      return;
+    }
+
+    updateBooks(token, formData).then(() => {
+      alert("レビューを更新しました");
+      navigate("/");
+    });
+  };
+  return (
+    <EditBooksPagePresenter
+      handleSubmit={handleSubmit(onUpdate)}
+      register={register}
+      errors={errors}
+    />
+  );
 };
--- src/pages/EditBooks/presenter.jsx
+++ src/pages/EditBooks/presenter.jsx
@@ -1,3 +1,67 @@
-export const EditBooksPagePresenter = () => {
-  return <div>編集ページ</div>;
+import React from "react";
+import { Header } from "../../components/Header";
+import "./editBooks.css"; // CSSファイルをインポート
+
+export const EditBooksPagePresenter = ({ register, handleSubmit, errors }) => {
+  return (
+    <>
+      <Header />
+      <div className="editBooks">
+        <h2>レビュー編集</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="formItem">
+            <label htmlFor="title">本のタイトル</label>
+            <input
+              type="text"
+              id="title"
+              className="input"
+              {...register("title", { required: "タイトルは必須です" })}
+            />
+            {errors.title && (
+              <span className="error-message">{errors.title.message}</span>
+            )}
+          </div>
+          <div className="formItem">
+            <label htmlFor="url">購入サイトのURL</label>
+            <input
+              type="url"
+              id="url"
+              className="input"
+              {...register("url", { required: "購入サイトのURLは必須です" })}
+            />
+            {errors.url && (
+              <span className="error-message">{errors.url.message}</span>
+            )}
+          </div>
+          <div className="formItem">
+            <label htmlFor="detail">本の詳細</label>
+            <textarea
+              id="detail"
+              className="textarea"
+              {...register("detail", { required: "本の詳細は必須です" })}
+            />
+            {errors.detail && (
+              <span className="error-message">{errors.detail.message}</span>
+            )}
+          </div>
+          <div className="formItem">
+            <label htmlFor="review">本のレビュー</label>
+            <textarea
+              id="review"
+              className="textarea"
+              {...register("review", { required: "本のレビューは必須です" })}
+            />
+            {errors.review && (
+              <span className="error-message">{errors.review.message}</span>
+            )}
+          </div>
+          <div className="updateButtonContainer">
+            <button className="updateButton" type="submit">
+              更新
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
